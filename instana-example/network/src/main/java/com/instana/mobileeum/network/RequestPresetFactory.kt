1ad3b0817d0b6db95d1820b5d6df1c28f950ebe5
Mikel Pascual <mikel@4rtstudio.com>
2020-04-16 13:04:29 +0100

    create network module in example

--- instana-example/network/src/main/java/com/instana/mobileeum/network/RequestPresetFactory.kt
+++ instana-example/network/src/main/java/com/instana/mobileeum/network/RequestPresetFactory.kt
@@ -6,49 +6,49 @@ package com.instana.mobileeum.network
 object RequestPresetFactory {
 
     val generalPresets = listOf(
-        RequestPreset(
+        com.instana.mobileeum.network.RequestPreset(
             name = "GET success",
             method = "GET",
             url = "https://4rtstudio.com",
             body = null
         ),
-        RequestPreset(
+        com.instana.mobileeum.network.RequestPreset(
             name = "GET failure",
             method = "GET",
             url = "https://httpstat.us/404",
             body = null
         ),
-        RequestPreset(
+        com.instana.mobileeum.network.RequestPreset(
             name = "GET exception",
             method = "GET",
             url = "https://httpstat-nonexistingurlhere.us/200",
             body = null
         ),
-        RequestPreset(
+        com.instana.mobileeum.network.RequestPreset(
             name = "GET ignored",
             method = "GET",
             url = "https://www.google.com",
             body = null
         ),
-        RequestPreset(
+        com.instana.mobileeum.network.RequestPreset(
             name = "POST success",
             method = "POST",
             url = "https://reqres.in/api/users",
             body = """{"name": "morpheus","job": "leader"}"""
         ),
-        RequestPreset(
+        com.instana.mobileeum.network.RequestPreset(
             name = "POST failure",
             method = "POST",
             url = "https://httpstat.us/403",
             body = """{"name": "morpheus","job": "leader"}"""
         ),
-        RequestPreset(
+        com.instana.mobileeum.network.RequestPreset(
             name = "PUT success",
             method = "PUT",
             url = "https://reqres.in/api/users/2",
             body = """{"name": "morpheus","job": "zion resident"}"""
         ),
-        RequestPreset(
+        com.instana.mobileeum.network.RequestPreset(
             name = "DELETE success",
             method = "DELETE",
             url = "https://reqres.in/api/users/2",
