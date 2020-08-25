ecbacd2c97eeb9dd1fcc00870f2cb974937a9715
Mikel Pascual <mikel@4rtstudio.com>
2020-08-25 13:13:38 +0100

    add minimum possible instrumented test to example app

---
+++ instana-example/app/src/androidTest/java/com/instana/mobileeum/ExampleInstrumentedTest.kt
@@ -0,0 +1,29 @@
+package com.instana.mobileeum
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun launchApp_noCrash() {
+        onView(withId(android.R.id.content))
+            .check(matches(isDisplayed()))
+    }
+}
