c5718d0916236c9a33cd8e1b29df4bcb8725277f
Christian Menschel <christian@tapwork.de>
2021-01-07 11:16:53 +0000

    Add first iOS specific code

--- ios/Classes/SwiftFlutterAgentPlugin.swift
+++ ios/Classes/SwiftFlutterAgentPlugin.swift
@@ -1,14 +1,31 @@
 import Flutter
 import UIKit
+import InstanaAgent
 
-public class SwiftFlutterAgentPlugin: NSObject, FlutterPlugin {
+public class SwiftInstanaFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_agent", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterAgentPlugin()
+    let instance = SwiftInstanaFlutterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    Instana.setup(key: InstanaKey, reportingURL: URL(string: InstanaURL)!)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if call.method == "getSessionID" {
+      result("Instana Session " + (Instana.sessionID ?? "No Session"))
+    } else if call.method == "getPlatformVersion" {
+      result("iOS " + UIDevice.current.systemVersion)
+    } else if call.method == "setView" {
+      guard let args = call.arguments as? [String: Any], let name = args["name"] as? String else {
+        return result(FlutterError(code: "UNAVAILABLE",
+                        message: "Arguments missing",
+                        details: nil))
+      }
+      Instana.setView(name: name)
+      result("View name set")
+    }
   }
 }
+
+let InstanaKey = "$(InstanaKey)"
+let InstanaURL = "$(InstanaURL)"
--- ios/flutter_agent.podspec
+++ ios/flutter_agent.podspec
@@ -3,21 +3,22 @@
 # Run `pod lib lint flutter_agent.podspec' to validate before publishing.
 #
 Pod::Spec.new do |s|
-  s.name             = 'flutter_agent'
-  s.version          = '0.0.1'
+  s.name             = 'InstanaFlutter'
+  s.version          = '1.0.0'
   s.summary          = 'Instana Agent for Flutter'
   s.description      = <<-DESC
 Instana Agent for Flutter
                        DESC
   s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Instana Inc.' => 'info@instana.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '8.0'
+  s.platform = :ios, '11.0'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
-  s.swift_version = '5.0'
+  s.swift_version = '5.3'
+  s.dependency 'InstanaAgent', '1.1.6'
 end
