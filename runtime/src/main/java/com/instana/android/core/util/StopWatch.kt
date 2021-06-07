aad962f8721d4603d9968de22379c89eb56d038a
Mikel Pascual <mikel@4rtstudio.com>
2021-06-07 17:48:27 +0100

    fix trace timing

--- runtime/src/main/java/com/instana/android/core/util/StopWatch.kt
+++ runtime/src/main/java/com/instana/android/core/util/StopWatch.kt
@@ -31,7 +31,7 @@ class StopWatch {
     }
 
     fun stop() {
-        if (startTime != 0L && endTime != 0L) {
+        if (startTime != 0L) {
             endTime = System.currentTimeMillis()
             elapsedTime = endTime - startTime
         }
